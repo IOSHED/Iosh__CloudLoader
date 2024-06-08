4b3d6bccbc66d2f49b199a68d94b5de455198adb
IOSH <iveninvala7@gmal.com>
2024-06-08 18:14:16 +0300

    Added documentation in the `check_alive` module, as well as in the public functions of the library itself `core_cloud_loader`

--- core/src/check_alive.rs
+++ core/src/check_alive.rs
@@ -1,4 +1,4 @@
-use futures::Future;
+use futures::{future, Future};
 use std::pin::Pin;
 
 use crate::{
@@ -18,8 +18,7 @@ pub trait CheckerCloud {
     ///
     /// # Return
     ///
-    /// - `Pin<Box<dyn Future<Output = bool> + Send>>`: Returns `true` if cloud service is alive, `false` otherwise.
-    ///
+    /// - `Pin<Box<dyn Future<Output = CheckAliveResult<bool>> + Send>>`: Returns `true` if cloud service is alive, `false` (or Error telling you why the token verification is not possible) otherwise.
     fn check(
         &self,
         cloud: Cloud,
@@ -44,6 +43,32 @@ impl CheckerCloud for NetCheckerCloud {
     }
 }
 
+/// Checks the availability of multiple cloud storage services using their OAuth tokens.
+///
+/// # Arguments
+///
+/// - `checker`: The structure implementing the trate `CheckerCloud`, which checks for the correctness of the OAuth token.
+/// - `clouds_with_auth`: A slice of tuples, where each tuple contains a `Cloud` enum and an `OAuthToken`.
+///
+/// # Returns
+///
+/// - `Vec<CheckAliveResult<bool>>`: Returns an array of bool values (or Error telling you why the token verification is not possible). Each index of the returned array corresponds to the index of the `clouds_with_auth argument`.
+///
+/// # Examples
+///
+/// ```no_run
+/// use core::check_alive::{NetCheckerCloud, Cloud, OAuthToken, are_all_clouds_alive};
+///
+/// let google_token = OAuthToken { token: "fake_google_oauth_token" };
+/// let yandex_token = OAuthToken { token: "fake_yandex_oauth_token" };
+/// let clouds = vec![
+///     (Cloud::GoogleDrive, google_token),
+///     (Cloud::YandexDisk, yandex_token),
+/// ];
+///
+/// let all_alive = are_all_clouds_alive(NetCheckerCloud, &clouds);
+/// assert_eq!(all_alive, [Ok(false), Ok(false)]);
+/// ```
 pub async fn are_all_clouds_alive<C>(
     checker: C,
     clouds_with_auth: &[(Cloud, OAuthToken)],
@@ -59,13 +84,35 @@ where
         })
         .collect();
 
-    let results = futures::future::join_all(tasks).await;
+    let results = future::join_all(tasks).await;
     results
         .into_iter()
         .map(|res| res.unwrap_or(Err(CheckAliveError::JoinTokioTaskFailed)))
         .collect()
 }
 
+/// Checks the availability of cloud storage service.
+///
+/// # Arguments
+///
+/// - `checker`: The structure implementing the trate `CheckerCloud`, which checks for the correctness of the OAuth token.
+/// - `cloud`: The cloud that is being checked for performance.
+/// - `tocken`: OAuth token for authorization on this cloud.
+///
+/// # Returns
+///
+/// - `CheckAliveResult<bool>`: Returns `true` if cloud service is alive, `false` (or Error telling you why the token verification is not possible) otherwise.
+///
+/// # Examples
+///
+/// ```no_run
+/// use core::check_alive::{NetCheckerCloud, Cloud, OAuthToken, is_cloud_alive};
+///
+/// let google_token = OAuthToken { token: "fake_google_oauth_token" };
+///
+/// let is_alive = is_cloud_alive(NetCheckerCloud, Cloud::GoogleDrive, google_token);
+/// assert_eq!(is_alive, Ok(false));
+/// ```
 pub async fn is_cloud_alive<C>(
     checker: C,
     cloud: Cloud,
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -8,18 +8,11 @@
 //!
 //! - `OAuthToken`: A structure to hold an OAuth token as a string slice.
 //! - `AddressFile`: A structure to associate a cloud service with a file path.
-//! - `LoaderInClouds`: A structure to thandle loading data in cloud services.
-//! - `LoaderFromClouds`: A structure to handle loading data from cloud services.
 //!
 //! # Functions
 //!
 //! - `are_all_clouds_alive`: Checks the availability of multiple cloud storage services using their OAuth tokens.
-//! - `is_cloud_alive`
-//!
-//! # Modules
-//!
-//! - `error`
-//!
+//! - `is_cloud_alive`: Checks the availability of cloud storage service.
 
 #![allow(clippy::all)]
 
@@ -29,16 +22,9 @@ mod error;
 mod loaders;
 mod prelude;
 
-pub mod tests {
-    pub use super::check_alive::{are_all_clouds_alive, is_cloud_alive, CheckerCloud};
-    pub use super::domain::{AddressFile, Cloud, OAuthToken};
-}
-
-pub use domain::{Cloud, OAuthToken};
+pub use domain::{AddressFile, Cloud, OAuthToken};
 pub use error::CheckAliveError;
 pub use prelude::CheckAliveResult;
-
-// TODO: Update docs
 
 /// Checks the availability of multiple cloud storage services using their OAuth tokens.
 ///
@@ -48,11 +34,11 @@ pub use prelude::CheckAliveResult;
 ///
 /// # Returns
 ///
-/// - `Vec<bool>`: Returns an array of bool values. Each index of the returned array corresponds to the index of the `clouds_with_auth argument`.
+/// - `Vec<CheckAliveResult<bool>>`: Returns an array of bool values (or Error telling you why the token verification is not possible). Each index of the returned array corresponds to the index of the `clouds_with_auth argument`.
 ///
 /// # Examples
 ///
-/// ```
+/// ```no_run
 /// use core::{Cloud, OAuthToken, are_all_clouds_alive};
 ///
 /// let google_token = OAuthToken { token: "fake_google_oauth_token" };
@@ -63,7 +49,7 @@ pub use prelude::CheckAliveResult;
 /// ];
 ///
 /// let all_alive = are_all_clouds_alive(&clouds);
-/// assert_eq!(all_alive, [false, false]);
+/// assert_eq!(all_alive, [Ok(false), Ok(false)]);
 /// ```
 pub async fn are_all_clouds_alive(
     clouds_with_auth: &[(Cloud, OAuthToken)],
@@ -81,16 +67,17 @@ pub async fn are_all_clouds_alive(
 ///
 /// # Returns
 ///
-/// - `bool`: Returns `true` if cloud service is alive, `false` otherwise.
+/// - `CheckAliveResult<bool>`: Returns `true` if cloud service is alive, `false` (or Error telling you why the token verification is not possible) otherwise.
 ///
 /// # Examples
-/// ```
+///
+/// ```no_run
 /// use core::{Cloud, OAuthToken, is_cloud_alive};
 ///
 /// let google_token = OAuthToken { token: "fake_google_oauth_token" };
 ///
-/// let is_alive = is_cloud_alive(&Cloud::GoogleDrive, &google_token);
-/// assert_eq!(is_alive, false);
+/// let is_alive = is_cloud_alive(Cloud::GoogleDrive, google_token);
+/// assert_eq!(is_alive, Ok(false));
 /// ```
 pub async fn is_cloud_alive(cloud: Cloud, tocken: OAuthToken) -> CheckAliveResult<bool> {
     use check_alive::NetCheckerCloud;
